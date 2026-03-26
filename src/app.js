16ea442f527b00a7868d0b944840edca7a0dc5c5
Therook-sudo <ezeliorae@gmail.com>
2026-03-26 09:34:10 +0100

    added prisma

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const prisma = require("./prisma");
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/db-status", async (req, res) => {
+  try {
+    await prisma.$queryRaw`SELECT 1`;
+    res.send("Database connection using Prisma is healthy!");
+  } catch (error) {
+    res.status(500).send("Database connection failed: " + error.message);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
